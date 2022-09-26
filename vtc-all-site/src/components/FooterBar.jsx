50c04b84d0ae670aa2f9d3aeab9745cede57f406
Wendy Hutson <hutson.wendy25@gmail.com>
2022-09-26 12:05:41 -0600

    FooterBar

---
+++ vtc-all-site/src/components/FooterBar.jsx
@@ -0,0 +1,19 @@
+export const FooterBar = () => {
+    return (
+
+                                <footer class="text-muted">
+                    <div class="container">
+                    <p class="float-right">
+                        <a href="#">Back to top</a>
+                    </p>
+                    <p>
+                        &copy; Valley Truck Centers
+                    </p>
+                    <p>
+                        <a href="index.html">www.valleytruckcenters.com</a>
+                    </p>
+                    </div>
+                    </footer>
+
+    )
+}
